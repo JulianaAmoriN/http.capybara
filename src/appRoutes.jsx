16252cc6115f3eb6b436b4872e57a3942569baa3
JulianaAmoriN <juamorim.sn@gmail.com>
2025-02-21 23:25:06 -0300

    feat(http): build

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Http from './pages/http/index';
 
 function AppRoutes() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<Http />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 };
 
