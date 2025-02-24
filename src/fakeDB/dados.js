a3cd70b47a50b6b371ea8a8b66b8384f4f6a13c7
JulianaAmoriN <juamorim.sn@gmail.com>
2025-02-24 13:50:03 -0300

    feat(http): criando prototipo

--- src/fakeDB/dados.js
+++ src/fakeDB/dados.js
@@ -1,4 +1,3 @@
-// enquanto não utilizo um banco de dados externo o array de http status code será salvo aqui;
 const statusCodes = [
     {
       "status": 100,
@@ -69,177 +68,237 @@ const statusCodes = [
         "status": 300,
         "legenda": "Multiple Choices",
         "imagem": "status300"
+    },
+    {
+        "status": 301,
+        "legenda": "Moved Permanently",
+        "imagem": "status301"
+    },
+    {
+        "status": 302,
+        "legenda": "Found",
+        "imagem": "status302"
+    },
+    {
+        "status": 303,
+        "legenda": "See Other",
+        "imagem": "status303"
+    },
+    {
+        "status": 304,
+        "legenda": "Not Modified",
+        "imagem": "status304"
+    },
+    {
+        "status": 307,
+        "legenda": "Temporary Redirect",
+        "imagem": "status307"
+    },
+    {
+        "status": 308,
+        "legenda": "Permanent Redirect",
+        "imagem": "status308"
+    },
+    {
+      "status": 400,
+      "legenda": "Bad Request",
+      "imagem": "status"
+    },
+    {
+      "status": 401,
+      "legenda": "Unauthorized",
+      "imagem": "status"
+    },
+    {
+      "status": 402,
+      "legenda": "Payment Required",
+      "imagem": "status"
+    },
+    {
+      "status": 403,
+      "legenda": "Forbidden",
+      "imagem": "status"
+    },
+    {
+      "status": 404,
+      "legenda": "Not Found",
+      "imagem": "status"
+    },
+    {
+      "status": 405,
+      "legenda": "Method Not Allowed",
+      "imagem": "status"
+    },
+    {
+      "status": 406,
+      "legenda": "Not Acceptable",
+      "imagem": "status"
+    },
+    {
+      "status": 407,
+      "legenda": "Proxy Authentication Required",
+      "imagem": "status"
+    },
+    {
+      "status": 408,
+      "legenda": "Request Timeout",
+      "imagem": "status"
+    },
+    {
+      "status": 409,
+      "legenda": "Conflict",
+      "imagem": "status"
+    },
+    {
+      "status": 410,
+      "legenda": "Gone",
+      "imagem": "status"
+    },
+    {
+      "status": 411,
+      "legenda": "Length Required",
+      "imagem": "status"
+    },
+    {
+      "status": 412,
+      "legenda": "Precondition Failed",
+      "imagem": "status"
+    },
+    {
+      "status": 413,
+      "legenda": "Content Too Large",
+      "imagem": "status"
+    },
+    {
+      "status": 414,
+      "legenda": "URI Too Long",
+      "imagem": "status"
+    },
+    {
+      "status": 415,
+      "legenda": "Unsupported Media Type",
+      "imagem": "status"
+    },
+    {
+      "status": 416,
+      "legenda": "Range Not Satisfiable",
+      "imagem": "status"
+    },
+    {
+      "status": 417,
+      "legenda": "Expectation Failed",
+      "imagem": "status"
+    },
+    {
+      "status": 418,
+      "legenda": "I'm a teapot",
+      "imagem": "status"
+    },
+    {
+      "status": 421,
+      "legenda": "Misdirected Request",
+      "imagem": "status"
+    },
+    {
+      "status": 422,
+      "legenda": "Unprocessable Content",
+      "imagem": "status"
+    },
+    {
+      "status": 423,
+      "legenda": "Locked",
+      "imagem": "status423"
+    },
+    {
+      "status": 424,
+      "legenda": "Failed Dependency",
+      "imagem": "status"
+    },
+    {
+      "status": 425,
+      "legenda": "Too Early",
+      "imagem": "status"
+    },
+    {
+      "status": 426,
+      "legenda": "Upgrade Required",
+      "imagem": "status"
+    },
+    {
+      "status": 428,
+      "legenda": "Precondition Required",
+      "imagem": "status"
+    },
+    {
+      "status": 429,
+      "legenda": "Too Many Requests",
+      "imagem": "status"
+    },
+    {
+      "status": 431,
+      "legenda": "Request Header Fields Too Large",
+      "imagem": "status"
+    },
+    {
+      "status": 451,
+      "legenda": "Unavailable For Legal Reasons",
+      "imagem": "status"
+    },
+    {
+      "status": 500,
+      "legenda": "Internal Server Error",
+      "imagem": "status500"
+    },
+    {
+      "status": 501,
+      "legenda": "Not Implemented",
+      "imagem": "status"
+    },
+    {
+      "status": 502,
+      "legenda": "Bad Gateway",
+      "imagem": "status"
+    },
+    {
+      "status": 503,
+      "legenda": "Service Unavailable",
+      "imagem": "status"
+    },
+    {
+      "status": 504,
+      "legenda": "Gateway Timeout",
+      "imagem": "status"
+    },
+    {
+      "status": 505,
+      "legenda": "HTTP Version Not Supported",
+      "imagem": "status"
+    },
+    {
+      "status": 506,
+      "legenda": "Variant Also Negotiates",
+      "imagem": "status"
+    },
+    {
+      "status": 507,
+      "legenda": "Insufficient Storage",
+      "imagem": "status"
+    },
+    {
+      "status": 508,
+      "legenda": "Loop Detected",
+      "imagem": "status"
+    },
+    {
+      "status": 510,
+      "legenda": "Not Extended",
+      "imagem": "status"
+    },
+    {
+      "status": 511,
+      "legenda": "Network Authentication Required",
+      "imagem": "status"
     }
-    // {
-    //   "status": 301,
-    //   "legenda": "Moved Permanently",
-    //   "imagem": "status301"
-    // },
-    // {
-    //   "status": 302,
-    //   "legenda": "Found",
-    //   "imagem": "status302"
-    // },
-    // {
-    //   "status": 303,
-    //   "legenda": "See Other",
-    //   "imagem": "status303"
-    // },
-    // {
-    //   "status": 304,
-    //   "legenda": "Not Modified",
-    //   "imagem": "status304"
-    // },
-    // {
-    //   "status": 307,
-    //   "legenda": "Temporary Redirect",
-    //   "imagem": "status307"
-    // },
-    // {
-    //   "status": 308,
-    //   "legenda": "Permanent Redirect",
-    //   "imagem": "status308"
-    // },
-    // {
-    //   "status": 400,
-    //   "legenda": "Bad Request",
-    //   "imagem": "status400"
-    // },
-    // {
-    //   "status": 401,
-    //   "legenda": "Unauthorized",
-    //   "imagem": "status401"
-    // },
-    // {
-    //   "status": 402,
-    //   "legenda": "Payment Required",
-    //   "imagem": "status402"
-    // },
-    // {
-    //   "status": 403,
-    //   "legenda": "Forbidden",
-    //   "imagem": "status403"
-    // },
-    // {
-    //   "status": 404,
-    //   "legenda": "Not Found",
-    //   "imagem": "status404"
-    // },
-    // {
-    //   "status": 405,
-    //   "legenda": "Method Not Allowed",
-    //   "imagem": "status405"
-    // },
-    // {
-    //   "status": 406,
-    //   "legenda": "Not Acceptable",
-    //   "imagem": "status406"
-    // },
-    // {
-    //   "status": 407,
-    //   "legenda": "Proxy Authentication Required",
-    //   "imagem": "status407"
-    // },
-    // {
-    //   "status": 408,
-    //   "legenda": "Request Timeout",
-    //   "imagem": "status408"
-    // },
-    // {
-    //   "status": 409,
-    //   "legenda": "Conflict",
-    //   "imagem": "status409"
-    // },
-    // {
-    //   "status": 410,
-    //   "legenda": "Gone",
-    //   "imagem": "status410"
-    // },
-    // {
-    //   "status": 411,
-    //   "legenda": "Length Required",
-    //   "imagem": "status411"
-    // },
-    // {
-    //   "status": 412,
-    //   "legenda": "Precondition Failed",
-    //   "imagem": "status412"
-    // },
-    // {
-    //   "status": 413,
-    //   "legenda": "Payload Too Large",
-    //   "imagem": "status413"
-    // },
-    // {
-    //   "status": 414,
-    //   "legenda": "URI Too Long",
-    //   "imagem": "status414"
-    // },
-    // {
-    //   "status": 415,
-    //   "legenda": "Unsupported Media Type",
-    //   "imagem": "status415"
-    // },
-    // {
-    //   "status": 416,
-    //   "legenda": "Range Not Satisfiable",
-    //   "imagem": "status416"
-    // },
-    // {
-    //   "status": 417,
-    //   "legenda": "Expectation Failed",
-    //   "imagem": "status417"
-    // },
-    // {
-    //   "status": 426,
-    //   "legenda": "Upgrade Required",
-    //   "imagem": "status426"
-    // },
-    // {
-    //   "status": 428,
-    //   "legenda": "Precondition Required",
-    //   "imagem": "status428"
-    // },
-    // {
-    //   "status": 429,
-    //   "legenda": "Too Many Requests",
-    //   "imagem": "status429"
-    // },
-    // {
-    //   "status": 431,
-    //   "legenda": "Request Header Fields Too Large",
-    //   "imagem": "status431"
-    // },
-    // {
-    //   "status": 451,
-    //   "legenda": "Unavailable For Legal Reasons",
-    //   "imagem": "status451"
-    // },
-    // {
-    //   "status": 500,
-    //   "legenda": "Internal Server Error",
-    //   "imagem": "status500"
-    // },
-    // {
-    //   "status": 501,
-    //   "legenda": "Not Implemented",
-    //   "imagem": "status501"
-    // },
-    // {
-    //   "status": 502,
-    //   "legenda": "Bad Gateway",
-    //   "imagem": "status502"
-    // },
-    // {
-    //   "status": 503,
-    //   "legenda": "Service Unavailable",
-    //   "imagem": "status503"
-    // },
-    // {
-    //   "status": 504,
-    //   "legenda": "Gateway Timeout",
-    //   "imagem": "status504"
-    // }
   ];
   
 export default statusCodes;
